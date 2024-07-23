cc3335d66b19d70fd2f12b875386427c7fe273c6
victor@nvda-jon <vjwang45@gmail.com>
2024-07-23 14:08:14 -0700

    bolster main.py tests

--- trading/config/test.py
+++ trading/config/test.py
@@ -3,5 +3,6 @@ from datetime import datetime
 
 CACHE_DIR = './tests/cache'
 SHOW_GRAPHS = False
+IS_DEBUG = True
 
 NOW = datetime(2023, 7, 22)
--- trading/tests/test_main.py
+++ trading/tests/test_main.py
@@ -18,16 +18,23 @@ class TestMain:
 
   @pytest.fixture
   def tickers(self):
-    return ['MDB', 'NVDA']
+    return ('NVDA',)
 
-  @freeze_time(config.FROZEN_TEST_DATE)
-  def test_scan(self, tickers, figman):
+  @freeze_time('2024-07-23')
+  def test_scan_success(self, tickers, figman):
     strat = Strategy.sell_intraquarter_derivatives
     scan(strat, tickers, figman)
     figman.render()
+  
+  @freeze_time(config.FROZEN_TEST_DATE)
+  def test_scan_move_threshold_error(self, figman):
+    strat = Strategy.sell_intraquarter_derivatives
+    tickers = ['MDB']
+    with pytest.raises(ValueError):
+      scan(strat, tickers, figman)
 
   @freeze_time(config.FROZEN_TEST_DATE)
-  def test_deep_dive_puts(self, tickers, figman):
+  def test_deep_dive_puts_success(self, tickers, figman):
     strat = deep_dive_puts
     strat(tickers, figman)
     figman.render()

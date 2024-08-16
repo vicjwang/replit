5dd657b7cd2ab4137a894c4d27394429d6c6ed05
victor@pixelbook <vjwang45@gmail.com>
2024-08-15 17:55:27 -0700

    Fix tests

--- trading/config/test.py
+++ trading/config/test.py
@@ -2,9 +2,11 @@ from datetime import datetime
 
 
 CACHE_DIR = './tests/saved'
-SHOW_GRAPHS = False
 IS_DEBUG = True
 MIN_ZSCORE = 0
 MY_WIN_PROBA = 0.90
+REGIME_START_DATE = '2023-01-01'
+SHOW_GRAPHS = False
+
 
 NOW = datetime(2024, 7, 25)

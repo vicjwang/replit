2c3708a74a0aba9831acddadf6c33cab109548c2
victor@nvda-jon <vjwang45@gmail.com>
2024-10-18 15:56:31 -0700

    Parallelize Scanner; reduce ratelimit to 60 calls per minute

--- trading/runners.py
+++ trading/runners.py
@@ -2,6 +2,8 @@ import traceback
 
 import config
 
+from joblib import Parallel, delayed
+
 from utils import strformat, get_win_proba
 
 
@@ -23,19 +25,21 @@ class Scanner(Runner):
     super().__init__(*args, **kwargs)
     self.win_proba = win_proba
 
-  def run(self, side=None):
-    for symbol in self.symbols:
-      try:
-        snapshot = self.build(symbol, self.win_proba).create_snapshot()
-        if snapshot:
-          self.figman.add_graph_as_ax(snapshot.graph_roi_vs_expiry)
-          print(strformat(snapshot.symbol, f"Adding subplot (WORTHY_MIN_BID={config.WORTHY_MIN_BID}, WORTHY_MIN_ROI={config.WORTHY_MIN_ROI})\n \"{snapshot.title}\"\n"))
+  def _run_iter(self, symbol):
+    try:
+      snapshot = self.build(symbol, self.win_proba).create_snapshot()
+      if snapshot:
+        self.figman.add_graph_as_ax(snapshot.graph_roi_vs_expiry)
+        print(strformat(snapshot.symbol, f"Adding subplot (WORTHY_MIN_BID={config.WORTHY_MIN_BID}, WORTHY_MIN_ROI={config.WORTHY_MIN_ROI})\n \"{snapshot.title}\"\n"))
 
-      except Exception as e:
-        print(strformat(symbol, f"Skipping - {e}"))
-        if config.IS_DEBUG:
-          traceback.print_exc()
-          raise e
+    except Exception as e:
+      print(strformat(symbol, f"Skipping - {e}"))
+      if config.IS_DEBUG:
+        traceback.print_exc()
+        raise e
+
+  def run(self, side=None):
+    Parallel(n_jobs=config.NUM_PARALLEL_JOBS)(delayed(self._run_iter)(symbol) for symbol in self.symbols)
 
 
 class Diver(Runner):
